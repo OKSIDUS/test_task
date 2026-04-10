91bf9f097ea491549f3d1a7f66956e13899bd589
Yaroslav <yaroslav@maximizesolutions.com>
2026-04-10 11:52:09 +0400

    Add XML attribute

--- TestTask/BookManager/Models/Book.cs
+++ TestTask/BookManager/Models/Book.cs
@@ -1,9 +1,17 @@
-﻿namespace BookManager.Models
+﻿using System.Xml.Serialization;
+
+namespace BookManager.Models
 {
+    [XmlRoot("Book")]
     public class Book
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
+        [XmlElement("Title")]
+        public string Title { get; set; } = string.Empty;
+        [XmlElement("Author")]
+        public string Author { get; set; } = string.Empty;
+        [XmlElement("Pages")]
         public int Pages { get; set; }
+
+        public Book() { }
     }
 }
---
+++ TestTask/BookManager/Models/BookCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace BookManager.Models
+{
+    [XmlRoot("Books")]
+    public class BookCollection
+    {
+        [XmlElement("Book")]
+        public List<Book> Items { get; set; } = new();
+    }
+}
